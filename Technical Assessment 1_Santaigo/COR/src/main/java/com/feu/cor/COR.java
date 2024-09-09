0c19e494e8c53338b93a2b471086be1b7df4b8bc
hmothe3rd <152071594+hmothe3rd@users.noreply.github.com>
2024-09-09 09:16:29 +0800

    Add files via upload

---
+++ Technical Assessment 1_Santaigo/COR/src/main/java/com/feu/cor/COR.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.feu.cor;
+
+/**
+ *
+ * @author 202310290
+ */
+public class COR {
+
+    public static void main(String[] args) {
+        System.out.println("Student #: 202310290 \t\t\t\t\t\t\tCollege:dCOMPUTER STUDIES    Program :BSITBA");
+        System.out.println("Name: SANTIAGO, HERMOGENES III BANGERO \t\t\t\t\tYear Level: 1    1st term, SY 20242025");
+        System.out.println("=========================================================================================================================================================");
+        System.out.println("Courses  | \t\t\tTitle \t\t\t\t|      Section   | Units | Days | \t\tTime \t\t      |     Room");
+        System.out.println("CCS0023  | OBJECT ORIENTED PROGRAMMING (LEC) \t\t\t| \tTC22 \t |   2\t |  F   |\t    07:00:00-09:40:00\t      |    ONLINE");
+        System.out.println("CCS0023L | OBJECT ORIENTED PROGRAMMING (LAB) \t\t\t| \tTC22 \t |   1\t |  M   |\t    07:00:00-09:50:00\t      |     E415"); 
+        System.out.println("GED0019  | UNDERSTANDING THE SELF \t\t\t\t| \tTC22 \t |   3\t | F/T  | 17:00:00-18:50:00/17:00:00-18:50:00 | ONLINE/ONLINE");
+        System.out.println("GED0021  | SPECIALIZED ENGLISH PROGRAM 2 \t\t\t| \tTC22 \t |   3\t | S/W  | 11:00:00-12:50:00/11:00:00-12:50:00 |   F503/F503");
+        System.out.println("IT0013   | NETWORKING 1 \t\t\t\t\t| \tTC22 \t |   3\t | S/TH | 07:00:00-08:50:00/07:00:00-09:50:00 |  E407/F1204");
+        System.out.println("IT0017   | DISCRETE MATHEMATICS \t\t\t\t| \tTC22 \t |   3\t | S/W  | 15:00:00-16:50:00/15:00:00-16:50:00 |   F612/F612");
+        System.out.println("IT0019   | QUANTITATIVE METHODS (INCL MODELING AND SIMULATION) \t| \tTC22 \t |   3\t | F/T  | 11:00:00-12:50:00/11:00:00-12:50:00 | ONLINE/ONLINE");
+        System.out.println("IT0119   | INFOAMTION MANAGMENT (LEC) \t\t\t\t| \tTC22 \t |   2\t |  T   |\t    13:00:00-15:40:00\t      |    ONLINE");
+        System.out.println("IT0119L  | INFOAMTION MANAGMENT (LAB) \t\t\t\t| \tTC22 \t |   1\t |  F   |\t    13:00:00-15:50:00\t      |    ONLINE");
+        System.out.println("=========================================================================================================================================================");
+        System.out.println("TOTAL UNITS 21");
+    }
+}
---
+++ Technical Assessment 1_Santaigo/MyInfo/src/main/java/com/feu/myinfo/MyInfo.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.feu.myinfo;
+
+/**
+ *
+ * @author 202310290
+ */
+public class MyInfo {
+
+    public static void main(String[] args) {
+        
+        int age = 20, year = 2023;
+        double height = 165.1;
+        float weight = 150.40f;
+        boolean bool = false;
+        String name = "Hermogenes Santaigo";
+        String gender = "Male";
+        String course = "Object-Oriented Programming";
+        
+        
+        /*System.out.println("Hello Pinas!");
+        System.out.println("Hello " + name);
+        System.out.println("My age is " + age);*/
+        System.out.println("Hi I am " + name + ". I am " + age + " years old. And I am " + gender + " \nThis year " +year+ ". I am studying the course "+course+" \nMy height is " +height+" inches and my weight is "+weight+ " \nWhat I belive is that I will pass OOP and that is " +!bool);
+    }
+}

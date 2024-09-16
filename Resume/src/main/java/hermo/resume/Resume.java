2bb7e67f525ad23cfe5e59b39528b356e121c9c4
hmothe3rd <152071594+hmothe3rd@users.noreply.github.com>
2024-09-16 08:40:46 +0800

    Add files via upload

---
+++ Resume/src/main/java/hermo/resume/Resume.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package hermo.resume;
+
+/**
+ *
+ * @author 202310290
+ */
+public class Resume {
+
+    public static void main(String[] args) {
+        System.out.println("Hermogenes B. Santiago lll");
+        System.out.println("Contact:\nGmail: snaknak44@gmail.com \nGithub: hmothe3rd \nNumber: 09604272172");
+        System.out.println("\nI am an ambitious individual pursuing a Bachelor's degree in Information Technology "
+                + "\nwith a specialization in Business Analytics. Currently in my first year, I am deeply "
+                + "\npassionate about leveraging technology to drive business insights and innovation. My journey "
+                + "\nin the field of Information Technology began with a fascination for how data can be transformed "
+                + "\ninto actionable insights. This passion led me to specialize in Business Analytics, where I am "
+                + "\nlearning to harness the power of data to solve complex business problems. In addition to my "
+                + "\nacademic pursuits, I am actively involved in various projects and internships that allow me to "
+                + "\napply my skills in real-world scenarios. These experiences have not only enhanced my technical "
+                + "\nabilities but also honed my problem-solving and analytical skills. Looking ahead, I am excited "
+                + "\nabout the endless possibilities that technology offers in the realm of business analytics. I am "
+                + "\ncommitted to continuous learning and staying updated with the latest trends and advancements in "
+                + "\nthe field to drive innovation and efficiency in business processes.");
+        System.out.println("\nEducations: \n -FEU Instetute of Technology, Philippines Manila \n  >B.S Information & Technology in Business analytics");
+        System.out.println("\nAwards/Certificates: \n -NASA International Space Apps Challenge Hackathon");        
+        System.out.println("\nExpertise: \n -Webdesign\n -Data Analytics");
+        
+    }
+}
---
+++ YourName/src/main/java/hermo/yourname/YourName.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package hermo.yourname;
+
+/**
+ *
+ * @author 202310290
+ */
+public class YourName {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java Programing Hermogenes B. Santiago");
+    }
+}
